f740f81b3ed8e1ba033a4209d3a70d0ae14ffe4c
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2022-03-21 22:33:52 +0700

    Tugas 4 PHP
    Tugas ini saya kirimkan untuk Team Niomic,Terimakasih.

---
+++ tugas4_1.php
@@ -0,0 +1,11 @@
+<?php
+$beli = 8000;
+$jual = 7500;
+$keuntungan = 300;
+$jawaban = 500 / $keuntungan;
+
+
+echo "Harga Beli = Rp $beli<br>";
+echo "Harga Jual = Rp $jual<br>";
+echo "Keuntungan = Rp $keuntungan<br>";
+echo "Jawaban : $jawaban ekor";
---
+++ tugas4_2.php
@@ -0,0 +1,9 @@
+<?php
+$jml_saluran = 523;
+$jml_terpakai = 8891;
+$rata_pakai = $jml_terpakai / $jml_saluran;
+
+echo "Jumlah Saluran = $jml_saluran<br>";
+echo "Jumlah Terpakai = $jml_terpakai<br>";
+echo "Rata-rata Pemakaian = $jml_terpakai / $jml_saluran<br>";
+echo "Jawaban : $rata_pakai liter/perkeluarga";
